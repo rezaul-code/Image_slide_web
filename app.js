6ac181e936a4c7e632c0305ddfc2c9a3203b3aca
Rezaul Karim Khan <rezaulkhan49688@gmail.com>
2024-11-24 21:43:04 +0530

    Files added

---
+++ app.js
@@ -0,0 +1,14 @@
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click',function(){
+    let item = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(item[0]);
+    
+});
+
+
+prev.addEventListener('click', function () {
+    let item = document.querySelectorAll('.item');
+    document.querySelector('.slide').insertBefore(item[item.length - 1], item[0]);
+});
